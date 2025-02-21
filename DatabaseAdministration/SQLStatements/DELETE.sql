9a392fdc17a0b7fab63887fc675af7d824c0ec2a
ItumelengMonyai <itumeleng.monyai@sambeconsulting.com>
2025-02-21 14:18:14 +0200

    Delete DatabaseAdministration directory

--- DatabaseAdministration/SQLStatements/DELETE.sql
+++
@@ -1,4 +0,0 @@
--- DELETE the record of an employee with EmployeeID = 1
-DELETE FROM Employees
-WHERE EployeeID = 1;
-GO
--- DatabaseAdministration/SQLStatements/INSERT.sql
+++
@@ -1,4 +0,0 @@
--- INSERT a new record into the Employees table
-INSERT INTO Employees (FirstName, LastName, Email, HireDate)
-Values('Itumeleng', 'Monyai', 'itumeleleng.monyai@gmail.com', '2025-02-17');
-GO
