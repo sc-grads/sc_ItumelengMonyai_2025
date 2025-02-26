2d02f0ca0d94d5009cab1b81e6bd30c3d3d807be
ItumelengMonyai <itumeleng.monyai@sambeconsulting.com>
2025-02-26 11:53:29 +0200

    Update Test1.sql

--- DatabaseAdministration/Test/Test1.sql
+++ DatabaseAdministration/Test/Test1.sql
@@ -1,6 +1,6 @@
-CREATE TABLE Users (
-    user_id INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    address VARCHAR(255)
+CREATE TABLE "Users" (
+    "User_ID" INT PRIMARY KEY,
+    "Name" VARCHAR(100),
+    "Email" VARCHAR(100),
+    "Address" VARCHAR(255)
 );
